d392376cee16c4db205e5158ea6952de66ec5261
pt7993@gmail.com <68317813+pt7993@users.noreply.github.com>
2021-02-27 03:32:21 +0900

    idea ignore

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.sun.org.apache.xpath.internal.operations.Mod;
@@ -9,15 +11,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -33,11 +42,22 @@ public class IndexController {
 
         return "posts-update";
     }
+
     @GetMapping("/posts/view/{id}")
     public String postsView(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
 
         return "posts-view";
+    }
+
+    @GetMapping("/normal/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/normal/join")
+    public String join() {
+        return "/join";
     }
 }
